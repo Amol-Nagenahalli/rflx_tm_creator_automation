68a9c0b1b145db055676e784446d804828314241
Amol-Nagenahalli <Amol.Nagenahalli@zebra.com>
2025-02-21 14:52:40 +0530

    headless
    headless

--- DataFolder/data.js
+++ DataFolder/data.js
@@ -1,59 +1,99 @@
 module.exports = {
 
+//Credentials :
   username: 'SYSADMIN',
   password: 'SYSADMINZebra@123!',
   Approverusername: 'APPROVER',
   Approverpassword: 'APPROVERZebra@123!',
-  projectTypeName: "Program Project : Sanity : KNLDEV01",
-  projectTypeNameApprover : "Program Project APPROVER : Sanity : KNLDEV01",
-  projectTypeDescription: "Program Project : Sanity : KNLDEV01",
+
+
+//Project Title
+
+//Program Project Title
   projectTitle: `Program Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   projectTitleApprover: `Program Project Sanity : Approver : RELEASE ${Math.floor(Math.random() * 100000)}`,
   projectTitleEdit : `Post Launch Edit Program Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   projectTitleCopy : `Copy Program Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   projectTitleEditCopy : "Post Launch Edit Program Project Sanity : TASK-RELEASE",
-  assignTo: "Store Manager - Store",
-  assignTo1: "Store Manager - Grocery",
-  creatorDept: "Corporate",
-  tags: "AutoProgramSanity",
-  tags2: "PostLaunchEdit",
-  projectNotes: "Notes For Project",
-  projectNotesEdit: "Post launch edit Notes",
-  attachmentFile: 'C:\\PlaywrightAutomation\\PlayWright\\Attachments\\file.pdf',
-  taskTitle: "Task 1",
-  taskTitle1: "Task 2",
-  specificStore: "AN1,7070,8080,ZebraX1,ZebraX2",
-  projectLaunchComment: "Project Launch",
-  simpleProjectType: "Simple Project - Update Store KNLDEV01",
+
+//Simple Project Title
   simpleProjectTitle: `Simple Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   SimpleProjectTitleEdit : `Post Launch Edit Simple Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   simpleRepeatingProjectTitle: `Simple Sanity Repeating : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   simpleRepeatingProjectTitleEdit:`EDIT Simple Sanity Repeating : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
-  collaborationProjectType: "Collaboration Project - KNLDEV01",
+
+//Collaboration Project Title
   collaborationProjectTitle: `Collaboration Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   collaborationProjectTitleEdit : `Post Launch Edit Collaboration Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
   collaboTaskStore: `Task ${Math.floor(Math.random() * 100000)} : Store`,
   collaboTaskStore1: `Task ${Math.floor(Math.random() * 100000)} : Store`,
   collaboTaskCorporate: `Task ${Math.floor(Math.random() * 100000)} : Corporate`,
+
+//Repeating Project Title
+  repeatingProjectTitle: `Repeating Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
+  repeatingProjectTitleEdit :`EDIT Repeating Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
+
+//Calendar Project Title
+  calendarProjectTitle:`Calendar Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
+  calendarProjectTitleEdit:`Post Edit Calendar Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
+
+  planProjectTitle:`Plan Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
+
+//Task Title
+  taskTitle: `Task 1 ${Math.floor(Math.random() * 100000)}`,
+  taskTitle1: `Task 2 ${Math.floor(Math.random() * 100000)}`,
+
+//Project Type Setup
+  projectTypeName: "Program Project : Sanity : KNLDEV01",
+  projectTypeNameApprover : "Program Project APPROVER : Sanity : KNLDEV01",
+  simpleProjectType: "Simple Project - Update Store KNLDEV01",
+  collaborationProjectType: "Collaboration Project - KNLDEV01",
+  simpleRepeatingType: "Simple Repeating Project - Update Store KNLDEV01",
+  repeatingProjectType: "Repeating Project : KNLDEV01",
+  planProjectType: "Plan For Plan Processing KNLDEV01",
+  calendarProjectType: "Calendar Project - KNLDEV01",
+
+
+//Assignment
+  assignTo: "Store Manager - Store",
+  assignTo1: "Store Manager - Grocery",
+  creatorDept: "Corporate",
+  assignToCorp: "Approver - Corporate",
+  corpStore: "CORP",
+  organizationLevel: "Corporate",
+
+
+//Tags
+  tags: "AutoProgramSanity",
+  tags2: "PostLaunchEdit",
+
+
+//Notes And Attachments
+  projectNotes: "Notes For Project",
+  projectNotesEdit: "Post launch edit Notes",
+  attachmentFile: 'C:\\PlaywrightAutomation\\PlayWright\\Attachments\\file.pdf',
+
+
+//Stores
+  specificStore: "AN1,7070,8080,ZebraX1,ZebraX2",
+
+
+//Comments
+  projectLaunchComment: "Project Launch",
+  postComment: "Changes",
+
+
+//Execution Level
   executionLevelStore: "Store",
   executionLevelCorporate: "Corporate",
   assignToCorporate : "Approver - Corporate",
-  simpleRepeatingType: "Simple Repeating Project - Update Store KNLDEV01",
-  repeatingProjectType: "Repeating Project : KNLDEV01",
-  repeatingProjectTitle: `Repeating Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
-  repeatingProjectTitleEdit :`EDIT Repeating Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
-  calendarProjectType: "Calendar Project - KNLDEV01",
-  calendarProjectTitle:`Calendar Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
-  calendarProjectTitleEdit:`Post Edit Calendar Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
-  planProjectType: "Plan For Plan Processing KNLDEV01",
-  planProjectTitle:`Plan Project Sanity : TASK-RELEASE ${Math.floor(Math.random() * 100000)}`,
+
+
+//Plan Resources
   planningDept: "Store",
   planCategory : "Product 6 - Recall",
   resourceType : "Store Manager",
   resourceDepartment: "Store",
-  postComment: "Changes",
-  organizationLevel: "Corporate",
-  assignToCorp: "Approver - Corporate",
-  corpStore: "CORP",
+
 
   };
