5a00a16559d957d3c752e417202aab5d5af85a54
Amol-Nagenahalli <114559471+Amol-Nagenahalli@users.noreply.github.com>
2025-02-24 13:29:21 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
       use: {
         browserName: 'chromium',
         channel: 'chrome',
-        headless: true,
+        headless: false,
       },
     },
     // {
